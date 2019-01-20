c4b9fe72b8623590974a4655cee6eefe7497b991
doginnn <diogenesemmanuel@gmail.com>
2019-01-20 09:42:03 -0300

    [UPD] Update em urls e views.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('posts', views.PostModelViewset)
 
 urlpatterns = [
-    path('', views.post_list, name= 'post_list')
+    path('', views.post_list, name= 'post_list'),
+    path('api/v1/', include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from rest_framework import viewsets
+from .serializers import PostSerializer
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
          
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+class PostModelViewset(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all().order_by('-title')
